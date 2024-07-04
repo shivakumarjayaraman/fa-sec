7c57d31bbe4b708e00e83190c3395a06de69f276
shiva-grantspro <shiva@Shivas-MacBook-Pro.local>
2024-07-04 21:31:13 +0530

    Added a comment for how to upload files from cmd line

--- upload.py
+++ upload.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+## Can do this from the command line too
+## http -f -b http://localhost:8000/small f@upload.py
+
 import requests
+
 
 url = "http://localhost:8000/user/small"
 file = {'f': open('upload.py', 'rb')}
